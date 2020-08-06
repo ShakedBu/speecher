3b2676c737b7b0d3fe38aecdd0c1eb2e74cc1f73
ShakedBu <buch.shaked@gmail.com>
2020-08-06 21:14:21 +0300

    api wrap

--- main.py
+++ main.py
@@ -2,9 +2,33 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hi')
-def print_hi():
-    return 'Speecher'
+@app.route('/')
+def default():
+    return 'Welcome to Speecher'
+
+
+@app.route('/speech/<query>', methods=['GET', 'POST'])
+def speech(query):
+    if Flask.method == 'GET':
+        return 'get speech'
+    else:
+        return 'create new speech'
+
+
+@app.route('/group/<query>', methods=['GET', 'POST'])
+def group(query):
+    if Flask.method == 'GET':
+        return 'get speech'
+    else:
+        return 'create new speech'
+
+
+@app.route('/phrase/<query>', methods=['GET', 'POST'])
+def phrase(query):
+    if Flask.method == 'GET':
+        return 'get speech'
+    else:
+        return 'create new speech'
 
 
 if __name__ == '__main__':
