d9e7a185a29b3af37b4d4fecce730f24fec7a008
ShakedBu <buch.shaked@gmail.com>
2020-09-27 11:20:29 +0300

    statistics returns char and words count

--- src/Routes/statistics.py
+++ src/Routes/statistics.py
@@ -2,7 +2,8 @@ from flask_restful import Resource
 from flask import request
 
 from src.DBUtils import execute_query
-from src.queries import GET_ALL_COUNTS
+from src.queries import GET_ALL_COUNTS, COUNT_CHARS_IN_WORD, COUNT_CHARS_IN_SENTENCE, COUNT_CHARS_IN_PARAGRAPH, \
+    COUNT_CHARS_IN_SPEECH, COUNT_WORDS_IN_SENTENCE, COUNT_WORDS_IN_PARAGRAPH, COUNT_WORDS_IN_SPEECH
 
 
 class Statistics(Resource):
@@ -37,11 +38,30 @@ def get_general_counts_by_speech(speech_id):
 
 
 def count_words(speech_id, paragraph, sentence):
-    return 0
+    if paragraph is not None:
+        if sentence is not None:
+            words = execute_query(COUNT_WORDS_IN_SENTENCE.format(speech_id, paragraph, sentence), True, True)
+        else:
+            words = execute_query(COUNT_WORDS_IN_PARAGRAPH.format(speech_id, paragraph), True, True)
+    else:
+        words = execute_query(COUNT_WORDS_IN_SPEECH.format(speech_id), True, True)
+
+    return words[0]
 
 
 def count_chars(speech_id, paragraph, sentence, word):
-    return 0
+    if paragraph is not None:
+        if sentence is not None:
+            if word is not None:
+                chars = execute_query(COUNT_CHARS_IN_WORD.format(speech_id, paragraph, sentence, word), True, True)
+            else:
+                chars = execute_query(COUNT_CHARS_IN_SENTENCE.format(speech_id, paragraph, sentence), True)
+        else:
+            chars = execute_query(COUNT_CHARS_IN_PARAGRAPH.format(speech_id, paragraph), True)
+    else:
+        chars = execute_query(COUNT_CHARS_IN_SPEECH.format(speech_id), True)
+
+    return chars[0]
 
 
 def count_words_appearances(speech_id):
